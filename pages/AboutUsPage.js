c96a4255fb6368502d6e52dfd6082676f348f495
Yit Soo <yjsoo004@mymail.sim.edu.sg>
2024-03-11 23:47:36 +0800

    modified:   pages/AboutUsPage.js 	modified:   components/Checklist.js 	modified:   pages/CalendarPage.js 	modified:   pages/HomePage.js 	modified:   pages/HomePage.test.js 	modified:   pages/HomeStorage.test.js 	modified:   pages/SecurePage.js 	modified:   pages/WelcomePage.js

--- pages/AboutUsPage.js
+++ pages/AboutUsPage.js
@@ -1,17 +1,16 @@
 import React, { useCallback} from 'react';
-import { StyleSheet, Text, View,ScrollView,TouchableOpacity, Linking } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Linking, Alert } from 'react-native';
 // Icons8 website url
-const icons8Url = 'https://icons8.com/';
+const icons8Link = 'https://icons8.com/';
 
 const Link = ({url}) => {
-  
-  // function to direct user to website when the link is pressed 
+  // to handle the link pressed by user
+  // to direct user to the external browser when the link is pressed 
   const handleLinkPress = useCallback(async () => {
-    // check if the link is supported
+    // checks whether the link is support
     const supported = await Linking.canOpenURL(url);
 
     if (supported) {
-      // open url in the mobile browser (direct to the browser by exit the application)
       await Linking.openURL(url);
     } else {
       Alert.alert(`URL is not supported: ${url}`);
@@ -19,11 +18,11 @@ const Link = ({url}) => {
   }, [url]);
 
   return(
-    <TouchableOpacity onPress={handleLinkPress}>
-      <Text style={styles.link}>
+    <Pressable onPress={handleLinkPress}>
+      <Text style={styles.icons8Link}>
         {url}
       </Text>
-    </TouchableOpacity>
+    </Pressable>
   )
 }
 
@@ -34,25 +33,20 @@ const AboutUsPage = () =>{
       accessible={true} 
       accessibilityLabel='About Us Page View'
     >
-      <ScrollView>
-        <View style={styles.aboutInfo}>
-          <Text style={styles.appName}>
-            Task Manager Application
-          </Text>
-          <Text>
-           This is a my Final Year Project Task Management Mobile Application. This app allows user to manage their daily task that cooperates the element of user friendly and aesthetics.
-          </Text>
-
-          {/*Icons by Icons8 */}
-          <View>
-            <Text style={styles.title}>
-              Icons by Icons8
-            </Text>
-            {/*touchable link that direct the user to the mealdb website in outer browser view */}
-            <Link url={icons8Url}/>
-          </View>
-        </View>
-      </ScrollView>
+      <View>
+        <Text style={styles.appName}>
+          Task Manager Application
+        </Text>
+        <Text style={styles.appDescrip}>
+          This is a my Final Year Project Task Management Mobile Application. 
+          This app allows user to manage their daily task that cooperates the element of user 
+          friendly and aesthetics.
+        </Text>
+        <Text style={styles.websiteName}>
+          Icons by Icons8
+        </Text>
+        <Link url={icons8Link}/>
+      </View>
     </View>
   );
 }
@@ -60,31 +54,27 @@ const AboutUsPage = () =>{
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    paddingTop: '8%',
-  },
-  // app info paragraph styling
-  aboutInfo: {
-    flex: 1,
-    margin: 27,
-    marginTop: 20,
+    paddingTop: '10%',
+    paddingHorizontal: '7%',
   },
   appName: {
-    marginBottom: 15,
-    fontSize: 30,
+    marginBottom: 18,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  // recipe source styling
-  title: {
+  appDescrip: {
+    fontSize: 16,
+  },
+  websiteName: {
     fontSize: 22,
     fontWeight: 'bold',
     marginVertical: 10,
   },
-  link: {
-    fontSize:15,
-    color:'#800080',
+  icons8Link: {
+    fontSize: 16,
+    color: '#800080',
   },
-
 });
 
 export default AboutUsPage;
